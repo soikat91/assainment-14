3fe05340d79141010f8511a9b9242a2cba3bcf4f
Soikat <mehfuzahmedbd@gmail.com>
2023-05-22 22:44:39 +0600

    final work done

--- OstadApp/routes/web.php
+++ OstadApp/routes/web.php
@@ -17,27 +17,27 @@ use Illuminate\Support\Facades\Route;
 
 
 // post route name
-Route::post('/name',[BookController::class,'createName']);
+// Route::post('/name',[BookController::class,'createName']);
 
 
-// get json Format route
-Route::get('/response',[BookController::class,'CreateJson']);
-// file upload route
-Route::post('/upload',[BookController::class,'photo']);
+// // get json Format route
+// Route::get('/response',[BookController::class,'CreateJson']);
+// // file upload route
+// Route::post('/upload',[BookController::class,'photo']);
 
 
-// cookie route
-Route::post('/Cookie',[BookController::class,'rememberToken']);
+// // cookie route
+// Route::post('/Cookie',[BookController::class,'rememberToken']);
 
-// header user agent route
-Route::post('/UserAgent',[BookController::class,'userAgent']);
+// // header user agent route
+// Route::post('/UserAgent',[BookController::class,'userAgent']);
 
-// page query parameter route
-Route::get('/page',[BookController::class,'page']);
+// // page query parameter route
+// Route::get('/page',[BookController::class,'page']);
 
 
-// post request route handler
-Route::post('/submit',[BookController::class,'createPost']);
+// // post request route handler
+// Route::post('/submit',[BookController::class,'createPost']);
 
 
 
